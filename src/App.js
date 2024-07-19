41442f1f904f6944fabc2d59b94a307e61d40fc9
Raven <priyadarsi02@gmail.com>
2024-07-19 21:19:51 +0530

    Added the cart modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Layout/Header.js";
 import Meals from "./components/Meals/Meals.js";
+import Cart from "./components/Cart/Cart.js";
 
 function App() {
+  const { cartIsShown, setCartIsShown } = useState(false);
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
