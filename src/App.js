9e479d213878d4fda966314611d2a646b230e0a7
Raven <priyadarsi02@gmail.com>
2024-07-21 16:02:05 +0530

    Huge changes
    > Added the cart function for the car to work properly.
    > Added more logic into the application so that datas can be moved and incresed in the cart system.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import Header from "./components/Layout/Header.js";
 import Meals from "./components/Meals/Meals.js";
 import Cart from "./components/Cart/Cart.js";
+import CartProvider from "./store/CartProvider.js";
 
 function App() {
-  const { cartIsShown, setCartIsShown } = useState(false);
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <Fragment>
-      <Cart />
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
